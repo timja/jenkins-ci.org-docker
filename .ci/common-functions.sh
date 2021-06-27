9d02944c362b305c641ccd3ade1f11ae400804da
Tim Jacomb <timjacomb1+github@gmail.com>
2021-06-27 15:07:20 +0100

    Merge

--- .ci/common-functions.sh
+++ .ci/common-functions.sh
@@ -1,6 +1,13 @@
 docker-login() {
-    # Making use of the credentials stored in `config.json`
-    docker login
+    if [ -n "$DOCKER_CONFIG" ]; then
+      # Making use of the credentials stored in `config.json`
+      echo "Using config.json"
+      docker login
+    else
+      # Using username and password variables
+      echo "Using username/password"
+      docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD}
+    fi
     echo "Docker logged in successfully"
 }
 
@@ -8,6 +15,10 @@ docker-enable-experimental() {
     # Enables experimental to utilize `docker manifest` command
     echo "Enabling Docker experimental...."
     export DOCKER_CLI_EXPERIMENTAL="enabled"
+}
+
+docker-debug-registries() {
+    cat $HOME/.docker/config.json | jq -r '.auths | keys'
 }
 
 docker-get-arch() {
